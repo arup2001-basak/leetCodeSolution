4e5d834ad855a8b848fcdb17bb3972b311483bf2
Arup Basak <65061893+arup2001-basak@users.noreply.github.com>
2022-07-11 10:35:08 +0530

    Time: 2 ms (57.31%), Space: 49.2 MB (21.36%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,42 +1,44 @@
 class Solution {
     public int trap(int[] height) {
-         int result = 0;
- 
-    if(height==null || height.length<=2)
-        return result;
- 
-    int left[] = new int[height.length];
-    int right[]= new int[height.length];
- 
-    //scan from left to right
-    int max = height[0];
-    left[0] = height[0];
-    for(int i=1; i<height.length; i++){
-        if(height[i]<max){
-            left[i]=max;
-        }else{
-            left[i]=height[i];
-            max = height[i];
+        int result = 0;
+        if(height == null || height.length <= 2){
+            return result;
         }
-    }
- 
-    //scan from right to left
-    max = height[height.length-1];
-    right[height.length-1]=height[height.length-1];
-    for(int i=height.length-2; i>=0; i--){
-        if(height[i]<max){
-            right[i]=max;
-        }else{
-            right[i]=height[i];
-            max = height[i];
+        
+        int n = height.length ;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        
+        //scan from left side
+        int max = height[0];
+        left[0] = height[0];
+        for(int i = 1; i<n; i++){
+            if(height[i]<max){
+                left[i] = max;
+            }
+            else{
+                left[i]=height[i];
+                max = height[i];
+            }
         }
-    }
- 
-    //calculate totoal
-    for(int i=0; i<height.length; i++){
+        
+        //scan from left side
+        max = height[n-1];
+        right[n-1]= height[n-1];
+        for(int i = n-2; i>=0; i--){
+            if(height[i] < max){
+                right[i] = max;
+            }
+            else{
+                right[i]=height[i];
+                max = height[i];
+            }
+        }
+        
+        for(int i=0; i<height.length; i++){
         result+= Math.min(left[i],right[i])-height[i];
     }
  
-    return result;
+        return result;
     }
 }
