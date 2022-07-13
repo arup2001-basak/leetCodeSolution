ade136dec10f7dab9fdf4078dab2e2fa24972e64
Arup Basak <65061893+arup2001-basak@users.noreply.github.com>
2022-07-13 10:35:07 +0530

    Time: 29 ms (13.95%), Space: 48 MB (33.44%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-          int n = board.length;
-        Set<Integer>[] rows = new Set[n];
-        Set<Integer>[] cols = new Set[n];
-        Set<Integer>[] blocks = new Set[n];
-        
-        for(int i = 0; i < n; i++) {
-            rows[i] = new HashSet<Integer>();
-            cols[i] = new HashSet<Integer>();
-            blocks[i] = new HashSet<Integer>();
-        }
-            
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (board[i][j] == '.') continue;                
-                int k = 3 * (i /3) + (j / 3);
-                
-                int num = board[i][j] - '0';
-                if (rows[i].contains(num)) return false;
-                if (cols[j].contains(num)) return false;
-                if (blocks[k].contains(num)) return false;
-                
-                rows[i].add(num);
-                cols[j].add(num);
-                blocks[k].add(num);
+         
+        HashSet seen = new HashSet();
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                if(board[i][j] != '.'){
+                    if(!seen.add("row"+i+board[i][j]) || !seen.add("column" +j + board[i][j])){
+                        return false;
+                    }
+                    if(!seen.add("box"+(i/3)*3+j/3+board[i][j])){
+                        return false;
+                    }
+                }
             }
-        }                
+        }
+        
         return true;
     }
 }
